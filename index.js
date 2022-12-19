56f09e750c46b86a12ad4addabc261654a539a9a
Sam Craig <samfcraig@gmail.com>
2022-12-19 18:58:29 -0500

    added Description input

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ inquirer
         {
             type: 'input',
             message: "What is the title of your project?",
+            name: "Project title",
         },
-
-
+        {
+            type: 'input',
+            message: 'What is the description of this project?',
+            name: "Description",
+        },
+        {
+            type: 'input',
+        }
 
 
     ])
