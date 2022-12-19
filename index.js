fe8d6549bcfee586aef03da0eb3fe1c94dd9065a
Sam Craig <samfcraig@gmail.com>
2022-12-19 18:17:19 -0500

    added starter inquirer code: first prompt and response to append file.

---
+++ index.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquierer');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "What is the title of your project?",
+        }
+
+
+
+    ])
+    .then((response) =>
+        fs.appendFile('README.md', `${JSON.stringify(response)}\n`
+        (err) =>
+        err ? console.log('Uh-oh!') : console.log('Nice! We did it!')));
