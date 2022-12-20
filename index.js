c5fb54a1b3f492bac5ce748707ae70af768eaa88
Sam Craig <samfcraig@gmail.com>
2022-12-20 13:21:30 -0500

    set up CreatePromptModule and changed WriteToFile

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const inquirer = require('inquierer');
 const fs = require('fs');
 
+const prompt = inquirer.createPromtModule();
+
+const writeToFile = (readme, projectTitle) => {
+    try {
+        fs.writeFileSync('README.md', readme);
+        console.log('Success!')
+    } catch (err) {
+        confirm.log(err);
+    }
+}
+
 inquirer
     .prompt([
         {
@@ -15,11 +26,12 @@ inquirer
         },
         {
             type: 'input',
-        }
-
-
+            message: 'What are the installation instructions?',
+            name: 'installation',
+        },
     ])
-    .then((response) =>
-        fs.appendFile('README.md', `${JSON.stringify(response)}\n`,
-        (err) =>
-        err ? console.log('Uh-oh!') : console.log('Nice! We did it!')));
+
+    // .then((response) =>
+    //     fs.appendFile('README.md', `${JSON.stringify(response)}\n`,
+    //     (err) =>
+    //     err ? console.log('Uh-oh!') : console.log('Nice! We did it!')));
