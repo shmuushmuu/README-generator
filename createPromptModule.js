3b6ab46a50d8dba95d61330b0122ce8c2d51061f
Sam Craig <samfcraig@gmail.com>
2022-12-20 18:39:07 -0500

    added email and gitHub questions

--- createPromptModule.js
+++ createPromptModule.js
@@ -4,65 +4,30 @@ inquirer
             type: 'input',
             message: "What is the title of your project?",
             name: "projectTitle",
-            validate: input => {
-                if (input) {
-                    return true;
-                } else {
-                    console.log('Unable to proceed. Please input a title now to continue.');
-                    return false;
-                }
-            }
         },
         {
             type: 'input',
             message: 'What is the description of this project?',
             name: "description",
-            validate: input => {
-                if (input) {
-                    return true;
-                } else {
-                    console.log('Unable to proceed. Please describe the project. ');
-                    return false;
-                }
-            }
         },
         {
             type: 'input',
             message: 'What are the installation instructions for this project?',
             name: 'installation',
-            validate: input => {
-                if (input) {
-                    return true;
-                } else {
-                    console.log('Unable to proceed. Please explain the installation instructions for this project.');
-                    return false;
-                }
-            }
         },
         {
             type: 'input',
             message: 'What is the usage information for this project?',
             name: 'usage',
-            validate: input => {
-                if (input) {
-                    return true;
-                } else {
-                    console.log('Unable to proceed. Please detail the usage information for this project.');
-                    return false;
-                }
-            }
         },
         {
             type: 'input',
-            message: 'What are the contribution guidelines for this project?',
-            name: 'guidelines',
-            validate: input => {
-                if (input) {
-                    return true;
-                } else {
-                    console.log('Unable to proceed. Please explain the contribution guidelines.');
-                    return false;
-                }
-            }
-        },
+            message: 'What is your e-mail address?',
+            name: 'email',
+        }
+        {
+            type: 'input',
+            message: 'What is your GitHub account?',
+            name: 'gitHub',
+        }
     ])
